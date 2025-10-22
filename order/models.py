dcd53d109b181432bd7e7e7a61878ac5e74a2245
yonaswondmagegn <yonaswondmagegn954@gmail.com>
2025-10-23 00:43:22 +0300

    test git

--- order/models.py
+++ order/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+User = get_user_model()
+
+
+class DesignType(models.Model):
+    name = models.CharField(max_length=225)
+
+class Order(models.Model):
+    posted_by = models.ForeignKey(User,on_delete=models.PROTECT)
+    design_type = models.ForeignKey(DesignType,on_delete=models.PROTECT)
+    order_code = models.AutoField(primary_key=True)
+    client = models.CharField(max_length=225)
+    contact   = models.CharField(max_length=13)
+    delivery_date = models.DateTimeField()
+    location = models.CharField(max_length=225)
+    full_payment = models.IntegerField()
+
+    
+
